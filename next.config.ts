4e216dd1c25e293772620f8040ae4aa09824e95c
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-05-09 18:00:30 +0000

    add backend connection setting to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from 'next';
+import type { Rewrite } from 'next/dist/lib/load-custom-routes';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites(): Promise<{ beforeFiles?: Rewrite[]; afterFiles?: Rewrite[]; fallback?: Rewrite[] } | Rewrite[]> {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://host.docker.internal:8000/api/:path*/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
