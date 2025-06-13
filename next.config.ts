80f6ee351ad8194eaf8ee840fbaa4b2b1f986945
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-06-13 12:09:05 +0000

    modify next.config.ts to work

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,36 @@
 
 // export default nextConfig;
 
-import type { NextConfig } from 'next';
-import type { Rewrite } from 'next/dist/lib/load-custom-routes';
+// import type { NextConfig } from 'next';
+// import type { Rewrite } from 'next/dist/lib/load-custom-routes';
 
-const nextConfig: NextConfig = {
-  async rewrites(): Promise<{ beforeFiles?: Rewrite[]; afterFiles?: Rewrite[]; fallback?: Rewrite[] } | Rewrite[]> {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://host.docker.internal:8000/api/:path*/',
-      },
-    ];
-  },
+// const nextConfig: NextConfig = {
+//   async rewrites(): Promise<{ beforeFiles?: Rewrite[]; afterFiles?: Rewrite[]; fallback?: Rewrite[] } | Rewrite[]> {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://host.docker.internal:8000/api/:path*/',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://host.docker.internal:8000/api/:path*/',
+            },
+        ]
+    },
 };
-
-export default nextConfig;
