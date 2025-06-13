bdcc654f8053cdb7d6a24b7a18fe9ad6a23700ac
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-06-13 15:44:41 +0000

    code 5-3-7

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -35,6 +35,22 @@ export default function Page() {
     setShownNewRow(false)
   };
 
+  // 更新・削除処理、更新・削除行の表示状態を保持
+  const [editingRow, setEditingRow] = useState(0);
+  const handleEditRow: any = (id: number) => {
+    setShownNewRow(false)
+    setEditingRow(id)
+  };
+  const handleEditCancel: any = (id: number) => {
+    setEditingRow(0)
+  };
+  const handleEdit: any = (id: number) => {
+    setEditingRow(0)
+  }
+  const handleDelete: any = (id: number) => {
+    setEditingRow(0)
+  }
+
   return (
     <>
       <h2>商品一覧</h2>
@@ -62,16 +78,31 @@ export default function Page() {
             </tr>
           ) : ""}
           {data.map((data: any) => (
-            <tr key={data.id}>
-              <td>{data.id}</td>
-              <td>{data.name}</td>
-              <td>{data.price}</td>
-              <td>{data.description}</td>
-              <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
-              <td>
-                <button>更新・削除</button>
-              </td>
-            </tr>
+            editingRow === data.id ? (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td><input type="text" defaultValue={data.name} /></td>
+                <td><input type="number" defaultValue={data.price} /></td>
+                <td><input type="text" defaultValue={data.description} /></td>
+                <td></td>
+                <td>
+                  <button onClick={handleEditCancel(data.id)}>キャンセル</button>
+                  <button onClick={handleEdit(data.id)}>更新する</button>
+                  <button onClick={handleDelete(data.id)}>削除する</button>
+                </td>
+              </tr>
+            ) : (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.price}</td>
+                <td>{data.description}</td>
+                <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
+                <td>
+                  <button onClick={handleEditRow(data.id)}>更新・削除</button>
+                </td>
+              </tr>
+            )
           ))}
         </tbody>
       </table>
