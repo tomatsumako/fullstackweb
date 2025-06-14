8feed9f4816362ca3342ee3ef0cc31030cd206ef
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-06-14 05:49:50 +0000

    code 5-4-4

--- app/inventory/products/[id]/page.tsx
+++ app/inventory/products/[id]/page.tsx
@@ -21,10 +21,9 @@ type InventoryData = {
   inventory: number;
 };
 
-export default function Page() {
-  // 商品IDにあたる検索条件
-  const params = { id: 1 };
-
+export default function Page({ params }) : {
+  params: { id: number },
+} {
   // 読込データを保持
   const [product, setProduct] = useState<ProductData>
   ({ id: 0, name: "", price: 0, description: ""});
