cc8c7c0e49c55e6282d30709fee5bd3a0e6f8fb4
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-09-01 16:11:32 +0000

    fix indent

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,63 +52,63 @@ export default function Page() {
 
     return (
         <ThemeProvider theme={defaultTheme}>
-        <Container component="main">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Typography component="h1" variant="h5">
-                    ログイン
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit(onSubmit)}>
-                {authError && (
-                    <Typography variant="body2" color="error">
-                        {authError}
-                    </Typography>
-                )}{" "}
-                <TextField
-                    type="text"
-                    id="username"
-                    variant="filled"
-                    label="ユーザー名（必須）"
-                    fullWidth
-                    margin="normal"
-                    {...register("username", { required: "必須入力です。" }) }
-                    error={Boolean(errors.username)}
-                    helperText={errors.username?.message?.toString() || ""}
-                />
-                <TextField
-                    type="password"
-                    id="password"
-                    variant="filled"
-                    label="パスワード（必須）"
-                    autoComplete="current-password"
-                    fullWidth
-                    margin="normal"
-                    {...register("password", {
-                        required: "必須入力です。",
-                        minLength: {
-                        value: 8,
-                        message: "8文字以上の文字列にしてください。",
-                        },
-                    })}
-                    error={Boolean(errors.password)}
-                    helperText={errors.password?.message?.toString() || ""}
-                />
-                <Button
-                    variant="contained"
-                    type="submit"
-                    fullWidth
-                    sx={{ mt: 3, mb: 2 }}
+            <Container component="main">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
                 >
-                    ログイン
-                </Button>
-                </Box>
+                    <Typography component="h1" variant="h5">
+                        ログイン
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+                        {authError && (
+                            <Typography variant="body2" color="error">
+                                {authError}
+                            </Typography>
+                        )}{" "}
+                        <TextField
+                            type="text"
+                            id="username"
+                            variant="filled"
+                            label="ユーザー名（必須）"
+                            fullWidth
+                            margin="normal"
+                            {...register("username", { required: "必須入力です。" }) }
+                            error={Boolean(errors.username)}
+                            helperText={errors.username?.message?.toString() || ""}
+                        />
+                        <TextField
+                            type="password"
+                            id="password"
+                            variant="filled"
+                            label="パスワード（必須）"
+                            autoComplete="current-password"
+                            fullWidth
+                            margin="normal"
+                            {...register("password", {
+                                required: "必須入力です。",
+                                minLength: {
+                                value: 8,
+                                message: "8文字以上の文字列にしてください。",
+                                },
+                            })}
+                            error={Boolean(errors.password)}
+                            helperText={errors.password?.message?.toString() || ""}
+                        />
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            fullWidth
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            ログイン
+                        </Button>
+                    </Box>
                 </Box>
             </Container>
         </ThemeProvider>
