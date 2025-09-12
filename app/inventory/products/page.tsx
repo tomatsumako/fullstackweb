f6313df3e8055b8288ce15e3548304186fdba2fe
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-09-12 18:21:14 +0000

    code 6-9-7

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -134,7 +134,9 @@ export default function Page() {
         setId(0);
     };
     const handleDelete = (id: number) => {
-        result('success', '商品が削除されました')
+        axios.delete(`/api/inventory/products/${id}`).then((response) => {
+            result('success', '商品が削除されました')
+        });
         setId(0);
     };
 
