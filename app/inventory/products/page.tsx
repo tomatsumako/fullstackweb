1e5c2f2188763cc50c6a5526a929d9a43e6dde05
Masayuki Yamaguchi <msykymgch@gmail.com>
2025-09-13 02:14:09 +0000

    code 6-9-9, 6-9-10, 6-9-11, 6-9-12

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -28,7 +28,7 @@ import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import productsData from "./sample/dummy_products.json";
-import axios from 'axios';
+import axios from '../../../plugins/axios';
 
 type ProductData = {
     id: number | null;
